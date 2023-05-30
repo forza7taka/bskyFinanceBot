21b70f5c3801e37a412b14b849226f2b61c80c6f
forza7taka <forza7.retainer.metropolis@gmail.com>
2023-05-30 23:41:32 +0900

    mod get method change

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,136 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import yahooFinance from 'yahoo-finance2';
-import axios from 'axios'
-import fs from 'fs'
+import axios from 'axios';
+import fs from 'fs';
+import alphavantage from 'alphavantage';
 
-interface symbolData {
+type Stock = {
   name: string;
   symbol: string;
   unit: string;
   format: string;
-}
+};
 
-interface session {
+type Currency = {
+  name: string;
+  fromCode: string;
+  toCode: string;
+  unit: string;
+  format: string;
+};
+
+interface Session {
   accessJwt: string;
   refreshJwt: string;
   handle: string;
   did: string;
 }
 
-dotenv.config()
+dotenv.config();
 
-const getBefore7day = async () => {
-  const yesterday = new Date()
-  yesterday.setDate(yesterday.getDate() - 7)
-  return getDate(yesterday)
+const getBefore7day = () => {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 7);
+  return getDate(yesterday);
+};
+
+const getToday = () => {
+  const today = new Date();
+  return getDate(today);
+};
+
+const getDate = (date: Date) => {
+  const year = date.getUTCFullYear();
+  const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+  const day = ('0' + date.getUTCDate()).slice(-2);
+  return `${year}-${month}-${day}`;
+};
+
+const fetchYahooFinance = async (symbols: Stock[]) => {
+  const before7day: string = await getBefore7day()
+  const queryOptions = { period1: before7day, /* ... */ };
+
+  let texts: string[] = [];
+  symbols.forEach(async (symbol) => {
+    console.log(symbol)
+    const results = await yahooFinance.historical(symbol.symbol, queryOptions)
+    console.log(results)
+    const yesterdayColose: number = results[0].close
+    const todayColose: number = results[1].close
+    const diff: number = todayColose - yesterdayColose
+    const ratio: number = Math.round(diff / yesterdayColose * 100 * 100) / 100
+    const text: string = symbol.format.replace("{0}", (Math.round(todayColose * 1000) / 1000).toString()).replace("{1}", (ratio > 0 ? "+" + ratio : ratio).toString())
+    console.log(text)
+    texts.push(text)
+  })
 }
 
-const getToday = async () => {
-  const today = new Date()
-  return getDate(today)
-}
+const fetchAlphaVantageExchange = async (symbols: Currency[]) => {
+  let texts: string[] = [];
+  const alpha = alphavantage({ key: process.env.APIKEY ? process.env.APIKEY : '' });
 
-const getDate = async (date: Date) => {
-  const year = date.getUTCFullYear()
-  const month = ('0' + (date.getUTCMonth() + 1)).slice(-2)
-  const day = ('0' + date.getUTCDate()).slice(-2)
-  return `${year}-${month}-${day}`
-}
+  for (const symbol of symbols) {
+    const data: any = await alpha.forex.rate(symbol.fromCode, symbol.toCode);
+    const rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+    const text = symbol.format.replace('{0}', (Math.round(rate * 1000) / 1000).toString());
+    texts.push(text);
+  }
 
+  return texts;
+};
 
 const run = async () => {
-
   try {
+    let texts: string[] = [];
 
-    const json = await fs.promises.readFile(process.argv[2], 'utf8');
-    const symbols: symbolData[] = JSON.parse(json);
-    const before7day: string = await getBefore7day()
-    const queryOptions = { period1: before7day, /* ... */ };
+    if ('exchange' === process.argv[2]) {
+      const json = fs.readFileSync(process.argv[3], 'utf8');
+      const symbols: Currency[] = JSON.parse(json);
+      texts = await fetchAlphaVantageExchange(symbols);
+    } else {
+      const json = fs.readFileSync(process.argv[3], 'utf8');
+      const symbols: Currency[] = JSON.parse(json);
 
-    let text: string = ""
-    symbols.forEach(async (symbol) => {
-      console.log(symbol)
-      const results = await yahooFinance.historical(symbol.symbol, queryOptions)
-      console.log(results)
-      const yesterdayColose: number = results[0].close
-      const todayColose: number = results[1].close
-      const diff: number = todayColose - yesterdayColose
-      const ratio: number = Math.round(diff / yesterdayColose * 100 * 100) / 100
-      text = symbol.format.replace("{0}", (Math.round(todayColose * 1000) / 1000).toString()).replace("{1}", (ratio > 0 ? "+" + ratio : ratio).toString())
-      console.log(text)
-    })
-    const response: any = await axios.post("https://bsky.social/xrpc/com.atproto.server.createSession", {
-      identifier: process.env.IDENTIFIER,
-      password: process.env.PASSWORD
-    })
-    const session: session = response.data
-    axios.defaults.headers.common['Authorization'] = `Bearer ` + session.accessJwt
-    await axios.post("https://bsky.social/xrpc/com.atproto.repo.createRecord", {
-      collection: "app.bsky.feed.post",
-      repo: session.did,
-      record: { text: text, createdAt: new Date() }
-    })
-
+    }
+    //    post(texts)
+    console.log(texts)
   } catch (error) {
     console.error(error);
   }
-}
+};
+
+const post = async (texts: string[]) => {
+  let contents = '';
+  for (const text of texts) {
+    contents += text;
+  }
+  if ('' === contents) {
+    return;
+  }
+  try {
+    const response: any = await axios.post('https://bsky.social/xrpc/com.atproto.server.createSession', {
+      identifier: process.env.IDENTIFIER,
+      password: process.env.PASSWORD,
+    });
+
+    const session: Session = response.data;
+    axios.defaults.headers.common['Authorization'] = `Bearer ` + session.accessJwt;
+    await axios.post('https://bsky.social/xrpc/com.atproto.repo.createRecord', {
+      collection: 'app.bsky.feed.post',
+      repo: session.did,
+      record: { text: contents, createdAt: new Date() },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const main = async () => {
-  await run();
+  try {
+    await run();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 main();
